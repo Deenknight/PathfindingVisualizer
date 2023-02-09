f52eb2ab5067cc2c70b929b4fe0ae2a39de19e22
Deenknight <Deenknight21@hotmail.com>
2023-02-08 23:47:21 -0700

    Basic React Application

---
+++ pathfinding-visualizer/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+// import { Form } from './components/Form';
+// {} are necessary for names exports
+// import {Greet} from './components/Greet';
+// import { Message } from './components/Message';
+// import { ClickHandler } from './components/ClickHandler';
+// import { UserGreeting } from './components/UserGreeting';
+// import { NameList } from './components/NameList';
+// import { ParentComponent } from './components/ParentComponent';
+// import { Stylesheet } from './components/Stylesheet';
+// import { Inline } from './components/Inline';
+// import NAMES from './components/data.json'
+import {useState} from 'react'
+
+
+function App() {
+  
+  return (
+    <div className="App">
+
+    </div> 
+  );
+}
+
+export default App;
