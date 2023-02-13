cc49a37222528bfed185b40b81afb883ad9b9432
Deenknight <Deenknight21@hotmail.com>
2023-02-12 22:29:55 -0700

    created the base pathfinder app

--- pathfinding-visualizer/src/App.js
+++ pathfinding-visualizer/src/App.js
@@ -1,25 +1,12 @@
-
+import React from 'react';
 import './App.css';
-// import { Form } from './components/Form';
-// {} are necessary for names exports
-// import {Greet} from './components/Greet';
-// import { Message } from './components/Message';
-// import { ClickHandler } from './components/ClickHandler';
-// import { UserGreeting } from './components/UserGreeting';
-// import { NameList } from './components/NameList';
-// import { ParentComponent } from './components/ParentComponent';
-// import { Stylesheet } from './components/Stylesheet';
-// import { Inline } from './components/Inline';
-// import NAMES from './components/data.json'
-import {useState} from 'react'
-
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
 
 function App() {
-  
   return (
     <div className="App">
-
-    </div> 
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
   );
 }
 
